89ea3ef25f2bb8e7bfe0f22f338c637b12203723
Tim Scully <hello@timscully.co.uk>
2020-05-15 15:34:06 +0100

    created sound for click, added background images

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var noOfDrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i < noOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click",
     function () {
-      alert("I got clicked");
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
     });
 }
