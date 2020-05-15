9928daf5a8f4434beed3fcc8e68431fcc288bce5
Tim Scully <hello@timscully.co.uk>
2020-05-15 14:12:30 +0100

    created loop to select all buttons

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-document.querySelector("button").addEventListener("click",
-function () {
-  alert("I got clicked");
-});
+var noOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",
+    function () {
+      alert("I got clicked");
+    });
+}
