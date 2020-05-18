780d80f6651d9b26a876b8d4bfdc8e774300a27c
Tim Scully <hello@timscully.co.uk>
2020-05-18 16:04:33 +0100

    can also separate function and call function name with out executing parenthesis within even listener

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-document.querySelector("button").addEventListener("click", function() {
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick() {
   alert("This has been clicked!");
-});
+}
