b452358ca6cd35c20cd32e879c35b55a39d46e0c
Tim Scully <hello@timscully.co.uk>
2020-05-19 13:24:39 +0100

    added default to switch statement for best practice

--- index.js
+++ index.js
@@ -7,13 +7,44 @@ for (var i = 0; i < noOfButtons; i++) {
       // find a variant of each button for the switch statement
       var buttonInnerHTML = this.innerHTML;
 
-      // switch expression is the thing we 'switch on'
+      // switch expression is the thing we 'switch on' so it knows what to look at for each 'case'
       switch (buttonInnerHTML) {
         case 'w':
           var audio = new Audio('sounds/kick-bass.mp3');
           audio.play();
           break;
-        default:
+
+        case 'a':
+          var audio = new Audio('sounds/snare.mp3');
+          audio.play();
+          break;
+
+        case 's':
+          var audio = new Audio('sounds/tom-1.mp3');
+          audio.play();
+          break;
+
+        case 'd':
+          var audio = new Audio('sounds/tom-2.mp3');
+          audio.play();
+          break;
+
+        case 'j':
+          var audio = new Audio('sounds/tom-3.mp3');
+          audio.play();
+          break;
+
+        case 'k':
+          var audio = new Audio('sounds/tom-4.mp3');
+          audio.play();
+          break;
+
+        case 'l':
+          var audio = new Audio('sounds/crash.mp3');
+          audio.play();
+          break;
+
+        default: console.log(buttonInnerHTML);
 
       }
 
