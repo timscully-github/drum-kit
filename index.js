03137ef37d76f5818dede292bd560e37644182fd
Tim Scully <hello@timscully.co.uk>
2020-05-19 14:37:30 +0100

    moved switch statement to its own function so we can call it separately

--- index.js
+++ index.js
@@ -1,52 +1,10 @@
-// document.querySelector("button").addEventListener("click", handleClick);
-
 var noOfButtons = document.querySelectorAll(".drum").length;
+
 for (var i = 0; i < noOfButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    document.querySelectorAll(".drum")[i].addEventListener("keydown", function () {
 
       // find a variant of each button for the switch statement
       var buttonInnerHTML = this.innerHTML;
-
-      // switch expression is the thing we 'switch on' so it knows what to look at for each 'case'
-      switch (buttonInnerHTML) {
-        case 'w':
-          var audio = new Audio('sounds/kick-bass.mp3');
-          audio.play();
-          break;
-
-        case 'a':
-          var audio = new Audio('sounds/snare.mp3');
-          audio.play();
-          break;
-
-        case 's':
-          var audio = new Audio('sounds/tom-1.mp3');
-          audio.play();
-          break;
-
-        case 'd':
-          var audio = new Audio('sounds/tom-2.mp3');
-          audio.play();
-          break;
-
-        case 'j':
-          var audio = new Audio('sounds/tom-3.mp3');
-          audio.play();
-          break;
-
-        case 'k':
-          var audio = new Audio('sounds/tom-4.mp3');
-          audio.play();
-          break;
-
-        case 'l':
-          var audio = new Audio('sounds/crash.mp3');
-          audio.play();
-          break;
-
-        default: console.log(buttonInnerHTML);
-
-      }
 
     });
 }
@@ -55,5 +13,49 @@ for (var i = 0; i < noOfButtons; i++) {
 // we can add event to the function parameter to show which event triggered the eventlistener, in this case which key was pressed
 // we can find 'key' as a property of event if we check in the console
 document.addEventListener("keydown", function (event) {
-  console.log(event.key);
+      console.log("This is one of the event keys: " + event.key);
 });
+
+
+function makeSound() {
+    // switch expression is the thing we 'switch on' so it knows what to look at for each 'case'
+    switch (buttonInnerHTML) {
+      case 'w':
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+        break;
+
+      case 'a':
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+        break;
+
+      case 's':
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+        break;
+
+      case 'd':
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play();
+        break;
+
+      case 'j':
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+        break;
+
+      case 'k':
+        var audio = new Audio('sounds/tom-4.mp3');
+        audio.play();
+        break;
+
+      case 'l':
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+        break;
+
+      default: console.log(buttonInnerHTML);
+
+    }
+}
